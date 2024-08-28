5543b943db673233288f3741afff7512ecee33ac
Raghavan <raghavan.work@outlook.com>
2024-08-28 07:25:14 +0530

    Add files via upload

---
+++ DiamondShape.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+ class DiamondShape {
+
+    public static void main(String[] args) {
+      
+        Scanner sn = new Scanner(System.in);
+
+        int n = sn.nextInt();
+
+        printdiamond(n);
+
+    }
+
+
+     static void printdiamond(int n) {
+        for (int i = 1; i <= n; i++) {
+       
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+           
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+//
+        for (int i = n - 1; i >= 1; i--) {
+  
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
